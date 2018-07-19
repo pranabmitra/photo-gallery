270229a0cfc71dd59acbe6e8cd64d8ea6b9c816b
Pranab <pranab.mitra@widespace.com>
2018-07-19 16:46:04 +0600

    implement phogrid page

--- client/entry.js
+++ client/entry.js
@@ -5,20 +5,25 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // import components
-import Main from './components/Main';
+import App from './components/App';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 
 // import react things
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+
+import store, { history } from './store'; 
 
 const router = (
-    <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path="/view/:postId" component={Single}></Route>
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={history }>
+            <Route path="/" component={App}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
 );
 
 render(router, document.getElementById('root'));
