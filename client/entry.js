a3e11d93c072fde1753044bdfa815f4140d351c3
Pranab <pranab.mitra@widespace.com>
2018-07-19 12:48:31 +0600

    setup starter kit from wesbos learn redux

---
+++ client/entry.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './components/Main';
+
+import css from './styles/style.styl';
+
+render(<Main />, document.getElementById('root'));
