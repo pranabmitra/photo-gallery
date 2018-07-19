ea0c599611cb8b8fe5b2e236eefbc26ebdcd57a1
Pranab <pranab.mitra@widespace.com>
2018-07-19 15:18:08 +0600

    added action creator

---
+++ client/actions/actionCreator.js
@@ -0,0 +1,26 @@
+// Increment Likes
+export function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index,
+    }
+}
+
+// add comment
+export function addComment(postId, author, commnet) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment,
+    }
+}
+
+// remove comment
+export function removeComment(postId, index) {
+    return {
+        type: 'REMOVE_COMMENT',
+        index,
+        postId,
+    }
+}
