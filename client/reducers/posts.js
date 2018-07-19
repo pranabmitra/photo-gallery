f8ca530a4ae16491730ade39fc8df0b57c8c6fb4
Pranab <pranab.mitra@widespace.com>
2018-07-19 16:56:17 +0600

    incrementing likes is now working

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,16 @@
 function posts(state = [], action) {
-    console.log(state, action);
-    return state;
+    console.log('Increasing Likes');
+    switch(action.type) {
+        case 'INCREMENT_LIKES':
+            const index = action.index;
+            return [
+                ...state.slice(0, index), // before all items that we are updating
+                {...state[index], likes: state[index].likes + 1},
+                ...state.slice(index + 1), // after all items that we are updating
+            ]
+        default:
+            return state;
+    }
 }
 
 export default posts;
